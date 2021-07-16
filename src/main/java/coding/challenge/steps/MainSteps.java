5f143cd6151d6974d3fea188fd17d0551fb66df6
Tadeusz S <ted.slawik@gmail.com>
2021-07-16 23:59:43 +0200

    basic env setup, page open

--- src/main/java/coding/challenge/steps/MainSteps.java
+++ src/main/java/coding/challenge/steps/MainSteps.java
@@ -4,15 +4,14 @@ import coding.challenge.pages.MainPage;
 import io.cucumber.java.en.Then;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MainSteps {
 
     MainPage mainPage = new MainPage();
     @Then("user should see {string} header")
     public void userShouldSeeCustomerHeader(String headerText) {
-
         mainPage.headerText().shouldBe(exactText(headerText));
-
-        // assertThat(mainPage.headerText().text()).isEqualTo(headerText);
+        assertThat(mainPage.headerText().text()).isEqualTo(headerText);
     }
 }
