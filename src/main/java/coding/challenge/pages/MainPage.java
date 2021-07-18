497366e84fe9f4227845a95c761d79cd08f27f19
Tadeusz S <ted.slawik@gmail.com>
2021-07-18 18:34:48 +0200

    logs, config file, few scenarios

--- src/main/java/coding/challenge/pages/MainPage.java
+++ src/main/java/coding/challenge/pages/MainPage.java
@@ -1,12 +1,45 @@
 package coding.challenge.pages;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class MainPage {
 
     public SelenideElement headerText(){
         return $(".container > h3");
+    }
+
+
+    public SelenideElement  searchDropDown() {
+        return $("#search-column");
+    }
+    public SelenideElement searchDropDownOption(String optionSelector){
+        return searchDropDown().$("option[value='"+optionSelector+"']");
+    }
+
+    public SelenideElement searchInput() {
+        return $("#search-input");
+    }
+
+    public ElementsCollection customersList() {
+        return $$("tbody >tr");
+    }
+
+    public SelenideElement resumeOfCustomerList() {
+        return $("#table-resume");
+    }
+
+    public SelenideElement matchCaseCheckbox() {
+        return $("#match-case") ;
+    }
+    public SelenideElement searchSloganResult(){
+        return  $("#search-slogan");
+    }
+
+    public SelenideElement clearButton() {
+        return $("#clear-button");
     }
 }
--- src/main/java/coding/challenge/steps/CommonSteps.java
+++ src/main/java/coding/challenge/steps/CommonSteps.java
@@ -1,15 +1,16 @@
 package coding.challenge.steps;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
 import static coding.challenge.allure.AllureReport.attachScreenshot;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CommonSteps {
-
-@When("user opens customers page")
+    @Given("user opened customer page")
+    @When("user opens customers page")
     public void userOpensMainPage() {
         open("");
-       attachScreenshot("main-page");
+        attachScreenshot("main-page");
     }
 }
